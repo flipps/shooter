4427c0ee42e03bda72dcdd895c0319eb50a451b5
Filipe Morais <fi.morais.mail@gmail.com>
2018-10-11 14:07:21 -0300

    Added sprite, still testing animations.

--- shooter.lua
+++ shooter.lua
@@ -1,36 +1,38 @@
 require 'bullets'
 require 'utils'
 
+local fps = 24
+
 shooter = {
   x = love.graphics.getWidth() / 2,
   y = love.graphics.getHeight() / 2,
+  image = love.graphics.newImage('assets/shooter.png'),
   xVelocity = 0,
   yVelocity = 0,
   radius = 30,
   angle = 0,
-  friction = 0.9,
-  speed = 30
+  friction = 0.8,
+  speed = 30,
+  animationTimer = 1 / fps,
+  totalFrames = 4,
+  xOffset = 0,
+  frame = 0
 }
+
+shooter.atlas = love.graphics.newImage('assets/shooter_atlas.png')
+shooter.sprite = love.graphics.newQuad(0, 0, 60, 60, shooter.atlas:getDimensions())
 
 function shooter:draw()
   love.graphics.setColor(1, 1, 0)
-  love.graphics.circle('fill', shooter.x, shooter.y, shooter.radius)
-
+  love.graphics.draw(shooter.atlas, shooter.sprite, shooter.x - shooter.image:getWidth() / 2, shooter.y - shooter.image:getHeight() / 2, shooter.angle - math.rad(-90), 1, 1, shooter.image:getWidth() / 2, shooter.image:getHeight() / 2)
+  -- love.graphics.circle('fill', shooter.x - shooter.image:getWidth() / 2, shooter.y - shooter.image:getHeight() / 2, shooter.radius)
   -- shooter angle indicator
-  local shooterCircleDistance = 20
-  local shooterCircleradius = 5
-
-  love.graphics.setColor(0, 0, 1)
-  love.graphics.circle(
-    'fill',
-    shooter.x + math.cos(shooter.angle) * shooterCircleDistance,
-    shooter.y + math.sin(shooter.angle) * shooterCircleDistance,
-    shooterCircleradius
-  )
+  -- local shooterCircleDistance = 30
+  -- local shooterCircleradius = 5
 
   -- bullets creation
   for y = -1, 1 do
-    for x = -1, 1 do 
+    for x = -1, 1 do
       for i,b in ipairs(bullets) do
         love.graphics.setColor(1, 0, 0)
         love.graphics.circle('fill', b.x, b.y, b.radius)
@@ -39,15 +41,15 @@ function shooter:draw()
   end
 
   -- Debbug
-  love.graphics.origin()
-  love.graphics.setColor(1, 0, 0)
-  love.graphics.print(table.concat({
-    'Shooter Angle: '..shooter.angle,
-    'Shooter x: '..shooter.x,
-    'Shooter y: '..shooter.y,
-    'Shooter x speed: '..shooter.xVelocity,
-    'Shooter y speed: '..shooter.yVelocity,
-  },'\n'), 10, 10)
+  -- love.graphics.origin()
+  -- love.graphics.setColor(1, 0, 0)
+  -- love.graphics.print(table.concat({
+  --   'Shooter Angle: '..shooter.angle,
+  --   'Shooter x: '..shooter.x,
+  --   'Shooter y: '..shooter.y,
+  --   'Shooter x speed: '..shooter.xVelocity,
+  --   'Shooter y speed: '..shooter.yVelocity,
+  -- },'\n'), 10, 10)
 end
 
 function shooter:update()
@@ -56,18 +58,15 @@ function shooter:update()
   local shooterSpeed = 100
 
   bullets.timer = bullets.timer - 1
+  shooter.animationTimer = shooter.animationTimer - dt
 
   --keyboard interaction
   if love.keyboard.isDown('d') then
     shooter.xVelocity = shooter.xVelocity + shooter.speed
-    -- shooter.angle = (shooter.angle + angleIndicatorSpeed * dt) % (2 * math.pi) -- keeps angles positive.
   elseif love.keyboard.isDown('a') then
     shooter.xVelocity = shooter.xVelocity - shooter.speed
-    -- shooter.angle = (shooter.angle - angleIndicatorSpeed * dt) % (2 * math.pi)
   elseif love.keyboard.isDown('w') then
     shooter.yVelocity = shooter.yVelocity - shooter.speed
-    -- shooter.xVelocity = shooter.xVelocity + math.cos(shooter.angle) * shooterSpeed * dt
-    -- shooter.yVelocity = shooter.yVelocity + math.sin(shooter.angle) * shooterSpeed * dt
   elseif love.keyboard.isDown('s') then
     shooter.yVelocity= shooter.yVelocity + shooter.speed
   end
@@ -85,7 +84,7 @@ function shooter:update()
 
   -- bullets
   for i,b in ipairs(bullets) do
-    local speed = 800
+    local speed = 1000
     
     -- removed bullets that are out of screen boundries
     if b.x > love.graphics.getWidth() or b.x < 0 then
@@ -97,18 +96,41 @@ function shooter:update()
     b.x = (b.x + math.cos(b.angle) * speed * dt)
     b.y = (b.y + math.sin(b.angle) * speed * dt)
   end
+  
+  -- shooter.sprite:setViewport(0, 0, 60, 60)
 
-  -- if love.keyboard.isDown('space') then
   if love.mouse.isDown(1) then
     if bullets.timer <= 0 then
       bullets.timer = 10
-      playSound(bullets.sound)
+      -- playSound(bullets.sound)
       table.insert(bullets, {
-        x = shooter.x + math.cos(shooter.angle) * shooter.radius,
-        y = shooter.y + math.sin(shooter.angle) * shooter.radius,
+        x = (shooter.x - shooter.image:getWidth() / 2) + math.cos(shooter.angle) * shooter.radius,
+        y = (shooter.y - shooter.image:getHeight() / 2) + math.sin(shooter.angle) * shooter.radius,
         radius = 5,
         angle = shooter.angle
-      })
+      }) 
+
+      for i = 0, shooter.totalFrames do
+        shooter.frame = shooter.frame + i + 1
+        shooter.sprite:setViewport(60 * shooter.frame, 0, 60, 60)
+
+        if i >= shooter.totalFrames then
+          shooter.sprite:setViewport(0, 0, 60, 60)
+        end
+      end
+
+      -- if shooter.animationTimer <= 0 then
+      --   shooter.animationTimer = 1 / fps
+      --   shooter.frame = shooter.frame + 1
+
+      --   if shooter.frame >= shooter.totalFrames then
+      --     shooter.frame = 0
+      --   end
+
+      --   shooter.xOffset = 60 * shooter.frame
+
+      --   shooter.sprite:setViewport(shooter.xOffset, 0, 60, 60)
+      -- end
     end
   end
 end
