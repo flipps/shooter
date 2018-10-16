51e8b5f05d2b404507dae5a55e5012f1d4de86de
Filipe Morais <fi.morais.mail@gmail.com>
2018-10-16 18:15:31 -0300

    AmbienceAudio Loop.

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ require 'shooter'
 
 function love.load()
   ambienceSound = love.audio.newSource('assets/And-the-Machines-Came-at-Midnight.mp3', 'static')
+  ambienceSound:setLooping(true)
   ambienceSound:play()
 end
 
